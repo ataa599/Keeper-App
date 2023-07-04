6526d95a6fe0c4ee431bc4455ac6d6f300daca2b
ataa599 <attasiddiqui101@gmail.com>
2023-07-04 20:17:14 +0500

    Keeper App part 2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,12 +2,25 @@ import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
+import notes from "../notes"
+
+function createNote(note){
+    return(
+    <Note 
+        key={note.key}
+        name={note.title}
+        content={note.content}
+    />);
+}
 
 function App(){
     return(
         <div>
             <Header />
-            <Note />
+            {/* <Note name="hello" content="xhejf" />
+            <Note name="hello" content="xhejf" /> */}
+            {/* <Note name={notes[0].title} content={notes[0].content} /> */}
+            {notes.map(createNote)}
             <Footer />
         </div>
     );
